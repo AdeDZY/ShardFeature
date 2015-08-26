833aa23290b11b167e6ba6498665fa4bf6a82c07
zhuyund <zhuyund@cs.cmu.edu>
2015-08-26 17:15:10 -0400

    changed gen_shard_lst_cent into functions

--- gen_shard_lst_cent.py
+++ gen_shard_lst_cent.py
@@ -19,84 +19,99 @@ def read_feat_file(filepath):
         term2feat[t] = (int(df), int(sum_tf), p)
     return term2feat, shard_size, shard_tf
 
-parser = argparse.ArgumentParser()
-parser.add_argument("partition_name")
-parser.add_argument("int_query_file", type=argparse.FileType('r'), help="queries in int format (queryid:queryterms)")
-parser.add_argument("--method", "-m", default="lm")
-parser.add_argument("--miu", "-i", type=float, default=0.0001)
-parser.add_argument("--lamb", "-l", type=float, default=500)
 
-args = parser.parse_args()
-
-base_dir = "/bos/usr0/zhuyund/partition/ShardFeature/output/" + args.partition_name
-
-queries = []
-for query in args.int_query_file:
-    query = query.strip()
-    query_id, query = query.split(":")
-    queries.append((query_id, query))
-
-res_dir = base_dir + "/rankings/"
-if not os.path.exists(res_dir):
-    os.makedirs(res_dir)
-
-shard_file = base_dir + "/shards"
-shards = []
-for line in open(shard_file):
-    shards.append(line.strip())
-
-# read in all feature files
-shards_features = {}
-shards_size = {}
-shards_tf = {}
-for shard in shards:
-    feat_file_path = "{0}/features/{1}.feat".format(base_dir, shard)
-    if not os.path.exists(feat_file_path):
-        shards_size[shard] = 0
-        shards_tf[shard] = 0
-        shards_features[shard] = {}
-        continue
-    feat, size, shard_tf = read_feat_file(feat_file_path)
-    shards_features[shard] = feat
-    shards_size[shard] = size
-    shards_tf[shard] = shard_tf
-
-# count number of shards with > 1000 documents
-n_valid_shards = 0
-
-# get reference model for smoothing
-ref_dv = {} # average of doc vectors
-ref = {} # tf_in_shard / total_tf_of_shard
-ndocs = 0
-nterms = 0
-for shard in shards:
-    feat = shards_features[shard]
-    size = shards_size[shard]
-    shard_tf = shards_tf[shard]
-
-    if size > 1000:
-        n_valid_shards += 1
-
-    ndocs += size
-    nterms += shard_tf
-    for term in feat:
-        df, sum_tf, sum_prob = feat[term]
-        ref_dv[term] = ref_dv.get(term, 0.0) + sum_prob * size
-        ref[term] = ref.get(term, 0.0) + float(sum_tf)
-for term in ref_dv:
-    ref_dv[term] /= ndocs
-    ref[term] /= nterms
+def get_ref(shards_features, shards_tf):
+    # get reference model for smoothing
+    ref = {}  # tf_in_shard / total_tf_of_shard
+    nterms = 0
+    for shard in shards_features:
+        feat = shards_features[shard]
+        shard_tf = shards_tf[shard]
+        nterms += shard_tf
+        for term in feat:
+            df, sum_tf, sum_prob = feat[term]
+            ref[term] = ref.get(term, 0.0) + float(sum_tf)
+    for term in ref:
+        ref[term] /= nterms
+    return ref
 
 
-for query_id, query in queries:
-    res = cent_kld.gen_lst(shards_features, ref_dv, ref, query, args.method, args.miu, args.lamb, shards_tf, shards_size)
+def get_ref_dv(shards_features, shards_size):
+    # get reference model for smoothing
+    ref_dv = {}  # average of doc vectors
+    ndocs = 0
+    for shard in shards_features:
+        feat = shards_features[shard]
+        size = shards_size[shard]
+        ndocs += size
+        for term in feat:
+            df, sum_tf, sum_prob = feat[term]
+            ref_dv[term] = ref_dv.get(term, 0.0) + sum_prob * size
+    for term in ref_dv:
+        ref_dv[term] /= ndocs
+    return ref_dv
 
-    outfile_path = "{0}/{1}_{2}.rank".format(res_dir, query_id, args.method)
-    outfile = open(outfile_path, 'w')
-    for score, shard in res:
-        outfile.write('{0} {1}\n'.format(shard, score))
-    outfile.close()
 
-valid_file = open(base_dir + "/nvalid", 'w')
-valid_file.write(str(n_valid_shards))
-valid_file.close()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("partition_name")
+    parser.add_argument("int_query_file", type=argparse.FileType('r'), help="queries in int format (queryid:queryterms)")
+    parser.add_argument("--method", "-m", default="lm")
+    parser.add_argument("--miu", "-i", type=float, default=0.0001)
+    parser.add_argument("--lamb", "-l", type=float, default=500)
+
+    args = parser.parse_args()
+
+    base_dir = "/bos/usr0/zhuyund/partition/ShardFeature/output/" + args.partition_name
+
+    queries = []
+    for query in args.int_query_file:
+        query = query.strip()
+        query_id, query = query.split(":")
+        queries.append((query_id, query))
+
+    res_dir = base_dir + "/rankings/"
+    if not os.path.exists(res_dir):
+        os.makedirs(res_dir)
+
+    shard_file = base_dir + "/shards"
+    shards = []
+    for line in open(shard_file):
+        shards.append(line.strip())
+
+    # read in all feature files
+    shards_features = {}
+    shards_size = {}
+    shards_tf = {}
+    for shard in shards:
+        feat_file_path = "{0}/features/{1}.feat".format(base_dir, shard)
+        if not os.path.exists(feat_file_path):
+            shards_size[shard] = 0
+            shards_tf[shard] = 0
+            shards_features[shard] = {}
+            continue
+        feat, size, shard_tf = read_feat_file(feat_file_path)
+        shards_features[shard] = feat
+        shards_size[shard] = size
+        shards_tf[shard] = shard_tf
+
+    # count number of shards with > 1000 documents
+    n_valid_shards = len([size for size in shards_size.values() if size >= 1000])
+
+    ref = get_ref(shards_features, shards_tf)
+    ref_dv = get_ref_dv(shards_features, shards_size)
+
+    for query_id, query in queries:
+        res = cent_kld.gen_lst(shards_features, ref_dv, ref, query, args.method, args.miu, args.lamb, shards_tf, shards_size)
+
+        outfile_path = "{0}/{1}_{2}.rank".format(res_dir, query_id, args.method)
+        outfile = open(outfile_path, 'w')
+        for score, shard in res:
+            outfile.write('{0} {1}\n'.format(shard, score))
+        outfile.close()
+
+    valid_file = open(base_dir + "/nvalid", 'w')
+    valid_file.write(str(n_valid_shards))
+    valid_file.close()
+
+main()
